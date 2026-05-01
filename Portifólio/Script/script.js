7ee0457dc5ec1dd02badd31e477997a81edc7da4
Rafael Vitor <barbosasaulo78@gmail.com>
2026-05-01 05:53:04 -0300

    Update dark mode styles and button text
    Change background color for dark mode and update button text.

--- Portifólio/Script/script.js
+++ Portifólio/Script/script.js
@@ -4,9 +4,10 @@ const botao = doc.getElementById("modoClaro/Escuro")
 let claro = true;
 botao.addEventListener("click", function() {
     if (claro) {
-        doc.body.style.backgroundColor = 'black';
+        doc.body.style.backgroundColor = '#141420';
         doc.body.style.color = 'white';
-        botao.textContent("Modo Escuro")
+        botao.textContent("Modo Escuro")
+
     } else {
         doc.body.style.backgroundColor = 'white';
         doc.body.style.color = 'black';
