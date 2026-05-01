8587a3f39c63e6ac31dd5c6bdc719f69193472b6
Rafael Vitor <barbosasaulo78@gmail.com>
2026-05-01 09:09:14 +0000

    Terminando Ajustes

--- Portifólio/Script/script.js
+++ Portifólio/Script/script.js
@@ -1,16 +1,16 @@
 const botao = document.getElementById("modoClaro/Escuro")
+var doc = document
 
 let claro = true;
 botao.addEventListener("click", function() {
     if (claro) {
         doc.body.style.backgroundColor = '#141420';
         doc.body.style.color = 'white';
-        botao.textContent("Modo Escuro")
-
+        botao.textContent = "Modo Claro"
     } else {
         doc.body.style.backgroundColor = 'white';
         doc.body.style.color = 'black';
-        botao.textContent("Modo Claro");
+        botao.textContent = "Modo Escuro"
     }
-claro =!claro
+    claro = !claro
 })
